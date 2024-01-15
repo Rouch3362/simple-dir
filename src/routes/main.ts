bd278f877d6af5216312a0f01001ec3c7cce47c1
unknown <rouchashoori@gmail.com>
2024-01-16 00:00:34 +0330

    added routes and their functionalities

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import fs from "fs"
+import path from "path";
 export const router = Router()
 
 
@@ -7,3 +9,41 @@ router.get("" , (req , res) => {
 })
 
 
+router.post("/create-subfolders" , (req , res) => {
+    let { path: desPath , parent , subcount , subname } = req.body
+    if (!desPath) {
+        desPath = "c://"
+    }
+    if (parent) {
+        fs.mkdir(path.join(desPath , parent) , (err) => {
+            if(err) {
+                console.log(err)
+            }
+        })
+    }
+    for (let i = 1; i <= subcount; i++) {
+        fs.mkdir(path.join(`${desPath}/${parent}` , `${subname} ${i}`) , (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    }
+})
+
+router.post('/create-customfolders' , (req , res) => {
+    let { names , path: desPath } = req.body
+    if (!names) {
+        return
+    }
+    names = names.split(",")
+    if (!desPath) {
+        desPath = "c://"
+    }
+    names.forEach(name => {
+        fs.mkdir(path.join(desPath , name) , (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    });
+})
