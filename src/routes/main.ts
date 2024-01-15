9da2da2f2aa2bfa1467bb72cc917ac5972a66780
unknown <rouchashoori@gmail.com>
2024-01-16 00:49:02 +0330

    added flash messages

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -4,8 +4,8 @@ import path from "path";
 export const router = Router()
 
 
-router.get("" , (req , res) => {
-    res.render('index.ejs')
+router.get("/" , (req , res) => {
+    res.render('index.ejs' , {message: req.flash("message")})
 })
 
 
@@ -17,14 +17,16 @@ router.post("/create-subfolders" , (req , res) => {
     if (parent) {
         fs.mkdir(path.join(desPath , parent) , (err) => {
             if(err) {
-                console.log(err)
+                req.flash("message" , "Somehting Went Wrong. Try Again")
+                return res.redirect("/")
             }
         })
     }
     for (let i = 1; i <= subcount; i++) {
         fs.mkdir(path.join(`${desPath}/${parent}` , `${subname} ${i}`) , (err) => {
             if (err) {
-                console.log(err)
+                req.flash("message" , "Somehting Went Wrong. Try Again")
+                return res.redirect("/")
             }
         })
     }
@@ -33,16 +35,18 @@ router.post("/create-subfolders" , (req , res) => {
 router.post('/create-customfolders' , (req , res) => {
     let { names , path: desPath } = req.body
     if (!names) {
-        return
+        req.flash("message" , "Folder Name Field Is Required. Please Fill It")
+        return res.redirect('/')
     }
     names = names.split(",")
     if (!desPath) {
         desPath = "c://"
     }
-    names.forEach(name => {
+    names.forEach((name: string) => {
         fs.mkdir(path.join(desPath , name) , (err) => {
             if (err) {
-                console.log(err)
+                req.flash("message" , "Something Went Wrong. Try Again")
+                return res.redirect("/")
             }
         })
     });
