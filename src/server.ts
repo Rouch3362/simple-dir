ddbd33c3c691d61bb1a1b97c5ce963f5daf14011
unknown <rouchashoori@gmail.com>
2024-01-16 00:50:42 +0330

    setting up session and flash

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,32 @@ import express from 'express'
 import 'dotenv/config'
 import bodyParser from 'body-parser'
 import {router} from './routes/main'
+import flash from 'connect-flash';
+import session from 'express-session';
 const port = process.env['PORT'] || 3000
 
 const app = express()
 
+
 app.set("view engine" , 'ejs')
 app.use(bodyParser.urlencoded({extended: false}))
 
+
+// setup express-session
+app.use(
+    session({
+      secret: process.env['SESSION_SECRET'],
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      },
+    })
+  );
+
+
+// setup flash
+app.use(flash())
 app.use("" , router)
 
 
