6ecbf396a839ab266fc9bc3e01d9b8f4b59d9970
unknown <rouchashoori@gmail.com>
2024-01-16 00:49:58 +0330

    compiled ts to js

--- dist/routes/main.js
+++ dist/routes/main.js
@@ -9,7 +9,7 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 exports.router = (0, express_1.Router)();
 exports.router.get("/", (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', { message: req.flash("message") });
 });
 exports.router.post("/create-subfolders", (req, res) => {
     let { path: desPath, parent, subcount, subname } = req.body;
@@ -19,14 +19,16 @@ exports.router.post("/create-subfolders", (req, res) => {
     if (parent) {
         fs_1.default.mkdir(path_1.default.join(desPath, parent), (err) => {
             if (err) {
-                console.log(err);
+                req.flash("message", "Somehting Went Wrong. Try Again");
+                return res.redirect("/");
             }
         });
     }
     for (let i = 1; i <= subcount; i++) {
         fs_1.default.mkdir(path_1.default.join(`${desPath}/${parent}`, `${subname} ${i}`), (err) => {
             if (err) {
-                console.log(err);
+                req.flash("message", "Somehting Went Wrong. Try Again");
+                return res.redirect("/");
             }
         });
     }
@@ -34,16 +36,18 @@ exports.router.post("/create-subfolders", (req, res) => {
 exports.router.post('/create-customfolders', (req, res) => {
     let { names, path: desPath } = req.body;
     if (!names) {
-        return;
+        req.flash("message", "Folder Name Field Is Required. Please Fill It");
+        return res.redirect('/');
     }
     names = names.split(",");
     if (!desPath) {
         desPath = "c://";
     }
-    names.forEach(name => {
+    names.forEach((name) => {
         fs_1.default.mkdir(path_1.default.join(desPath, name), (err) => {
             if (err) {
-                console.log(err);
+                req.flash("message", "Something Went Wrong. Try Again");
+                return res.redirect("/");
             }
         });
     });
--- dist/server.js
+++ dist/server.js
@@ -7,9 +7,22 @@ const express_1 = __importDefault(require("express"));
 require("dotenv/config");
 const body_parser_1 = __importDefault(require("body-parser"));
 const main_1 = require("./routes/main");
+const connect_flash_1 = __importDefault(require("connect-flash"));
+const express_session_1 = __importDefault(require("express-session"));
 const port = process.env['PORT'] || 3000;
 const app = (0, express_1.default)();
 app.set("view engine", 'ejs');
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+// setup express-session
+app.use((0, express_session_1.default)({
+    secret: process.env['SESSION_SECRET'],
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    },
+}));
+// setup flash
+app.use((0, connect_flash_1.default)());
 app.use("", main_1.router);
 app.listen(port, () => console.log(`Server Is Listening On Port ${port}`));
